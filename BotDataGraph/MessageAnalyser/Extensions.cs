e673194db58ae478c5591c13aae02772905368ba
Jordan Kelwick <jordankelwick@gmail.com>
2018-02-05 23:43:44 +0000

    Words and chars!

---
+++ BotDataGraph/MessageAnalyser/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BotDataGraph.MessageAnalyser
+{
+    public static class Extensions
+    {
+        public static IEnumerable<string> ToWords(this string s)
+        {
+            var separators = s.ToCharArray()
+                .Where(char.IsSeparator)
+                .Append('-')
+                .Distinct()
+                .ToArray();
+            var punctuation = s
+                .Where(char.IsPunctuation)
+                .Distinct()
+                .ToArray();
+
+            return s.Split(separators).Select(word => word.Trim(punctuation)).Where(word => !string.IsNullOrEmpty(word));
+        }
+    }
+}
--- BotDataGraph/MessageAnalyser/Neo4JMessageHandler.cs
+++ BotDataGraph/MessageAnalyser/Neo4JMessageHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,6 @@ using Neo4j.Driver.V1;
 
 namespace BotDataGraph.MessageAnalyser
 {
-    using System.Collections.Generic;
 
     public class Neo4JMessageHandler
     {
@@ -38,38 +38,90 @@ namespace BotDataGraph.MessageAnalyser
         {
             using (var session = this.driver.Session())
             {
-                var messageNodeId = await AddMessageNode(message, session);
+                var messageNodeId = await AddMessageNodeAsync(message, session);
 
-                //var words = message.Content.Split(' ');
-                //foreach (var word in words)
-                //{
-                //    await
-                //}
+                var words = message.Content.ToLowerInvariant().ToWords();
+
+                foreach (var word in words)
+                {
+                    var wordNodeId = await AddWordConnectionAsync(word, messageNodeId, session);
+
+                    foreach (var symbol in word)
+                    {
+                        await AddSymbolConnectionAsync(symbol, wordNodeId, session);
+                    }
+                }
             }
         }
 
-        private async Task<int> AddMessageNode(Message message, ISession session)
+        // TODO: Make async
+        private async Task<int> AddSymbolConnectionAsync(char symbol, int wordNodeId, ISession session)
+        {
+            return session.WriteTransaction(
+                tx =>
+                {
+                    var reader = tx.Run(
+                        "MATCH (w:Word) WHERE id(w) = $wordNodeId " +
+                        "MERGE (s:Symbol {name: $symbol}) " +
+                        "MERGE (w)-[r:HAS_SYMBOL]->(s) " +
+                        "return id(s) ",
+                        new {symbol, wordNodeId});
+
+                    return reader.First()[0].As<int>();
+                });
+        }
+
+        // TODO: Make async
+        private async Task<int> AddWordConnectionAsync(string word, int messageNodeId, ISession session)
+        {
+            return session.WriteTransaction(
+                tx =>
+                {
+                    var reader = tx.Run(
+                        "MATCH (m:Message) WHERE id(m) = $messageNodeId " +
+                        "MERGE (w:Word {name: $word}) " +
+                        "MERGE (m)-[r:HAS_WORD]->(w) " +
+                        "return id(w) ",
+                        new { word, messageNodeId });
+
+                    return reader.First()[0].As<int>();
+                });
+        }
+
+        // TODO: Make async
+        private async Task<int> AddMessageNodeAsync(Message message, ISession session)
         {
             var lastMessageNodeId = session.ReadTransaction(
                 tx =>
                 {
-
                     var reader = tx.Run(
                         "MATCH (c:Channel {channelId: $channelId})-[]-(message:Message) "
                         + "return id(message) "
                         + "ORDER BY message.time DESC "
                         + "LIMIT 1",
                         message.Parameters(this.startupTime));
-
-                    return reader.Single()[0].As<int>();
+                    try
+                    {
+                        return reader.First()[0].As<int>();
+                    }
+                    catch
+                    {
+                        return -1;
+                    }
                 });
 
-            return session.WriteTransaction(
-                tx =>
+            return session.WriteTransaction(tx =>
                 {
+                    var query1 = string.Empty;
+                    var query2 = string.Empty;
+                    if (lastMessageNodeId != -1)
+                    {
+                        query1 = "match (mOld:Message) WHERE ID(mOld) = $lastNodeId ";
+                        query2 = "MERGE (mOld)-[nextMessage:NEXT_MESSAGE]->(m)";
+                    }
                     // TODO: Separate out query to check for user and channel existence then create them separately
                     var reader = tx.Run(
-                        "match (mOld:Message) WHERE ID(mOld) = $lastNodeId " +
+                        query1 +
                         "MATCH (startup:Startup { time: $startupTime})" +
                         "MERGE (u:User {userId: $userId})" +
                         "MERGE (s:Server { serverId: $serverId})" +
@@ -79,12 +131,14 @@ namespace BotDataGraph.MessageAnalyser
                         "MERGE (m)-[channelRelate:IN_CHANNEL]->(channel)" +
                         "MERGE (u)-[su:SERVER_USER]-(s)" + 
                         "MERGE (s)-[sc:SERVER_CHANNEL]->(channel)" +
-                        "MERGE (mOld)-[nextMessage:NEXT_MESSAGE]->(m)" +
+                        query2 +
                         "return id(m)",
                         message.Parameters(this.startupTime, lastMessageNodeId));
 
                     return reader.Single()[0].As<int>();
                 });
         }
+
+        
     }
 }
