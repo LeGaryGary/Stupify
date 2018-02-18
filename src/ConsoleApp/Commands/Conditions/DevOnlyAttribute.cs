7ced7b27cd937bedfac3282095c706bebeb1a0dd
Jordan Kelwick <jordankelwick@gmail.com>
2018-02-18 14:34:42 +0000

    Preconditions

---
+++ src/ConsoleApp/Commands/Conditions/DevOnlyAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace StupifyConsoleApp.Commands.Conditions
+{
+    public class DevOnlyAttribute : PreconditionAttribute
+    {
+
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            if (context.User is IGuildUser user && user.RoleIds.Contains(Config.DeveloperRole))
+            {
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            }
+
+            return Task.FromResult(PreconditionResult.FromError("Trying to be sneaky eh? sorry only devs can do that :^)"));
+        }
+    }
+}
