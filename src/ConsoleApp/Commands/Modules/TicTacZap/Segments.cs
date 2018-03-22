d54e862dd79437642eec36509f0ca64d65583ff9
Sina Kamali <sina.kamali76@gmail.com>
2018-03-22 23:24:50 +0430

    cleanup

--- src/ConsoleApp/Commands/Modules/TicTacZap/Segments.cs
+++ src/ConsoleApp/Commands/Modules/TicTacZap/Segments.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Discord;
 using Discord.Commands;
 using StupifyConsoleApp.Client;
 using StupifyConsoleApp.DataModels;
