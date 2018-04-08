66648dbf107d4faa8cb55536358a8be321b0ad39
Jordan Kelwick <jordankelwick@gmail.com>
2018-04-08 10:36:36 +0100

    WORK GAWD DAMIT

--- Stupify.Data.SQL/Models/SegmentTemplate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Stupify.Data.SQL.Models
-{
-    public class SegmentTemplate
-    {
-        public int SegmentTemplateId { get; set; }
-        public virtual User User { get; set; }
-        public string Name { get; set; }
-    }
-}
