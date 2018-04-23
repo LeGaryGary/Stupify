b19c4a8637b7b8d0efa812516d011dc54ddcfb43
Jordan Kelwick <jordankelwick@gmail.com>
2018-04-23 22:51:13 +0100

    External access!

--- src/StupifyWebsite/Program.cs
+++ src/StupifyWebsite/Program.cs
@@ -13,6 +13,7 @@ namespace StupifyWebsite
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000")
                 .Build();
     }
 }
