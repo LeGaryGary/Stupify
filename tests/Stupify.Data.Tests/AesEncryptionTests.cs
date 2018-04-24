20e77c3f4a37a80175022e886f074f62f95d328e
Jordan Kelwick <jordankelwick@gmail.com>
2018-04-24 12:53:39 +0100

    Fixed

--- tests/Stupify.Data.Tests/AesEncryptionTests.cs
+++ tests/Stupify.Data.Tests/AesEncryptionTests.cs
@@ -13,14 +13,14 @@ namespace Stupify.Data.Tests
         [InlineData("😁😁😁҂֍⌇", "Password1")]
         [InlineData("Just a normal string ya know!", "😁😁😁҂֍⌇")]
         [InlineData("Just a normal string ya know!", "fBbaxzHxg2kZdKYqF%#&!5mSZaASq=yeeSFfT6m$5@Fhmq5TJuCSR*FP8?gQsssL+KRybS&-HPf@JJca3XM^$Rkf@?eW$ALGMDMX@w$YkGWP#%4T9Hus@LdjHwwcDR7kh+-2X^_=DSc6SuhgT8attV-Ya5G_QcK!mFQ73dZZuq6$QY+-427Av$Qm9xb6H3j6HQ?5p3SA3Y^xmzaAV5UhdJK2EDKdC!4+6rrEdUkG2nN5jhwmJddTE?%tDdd&g^p_")]
-        public void AesTests(string painText, string password)
+        public void AesTests(string plainText, string password)
         {
             var aes = new AesCryptography(password);
 
-            var encrypted = aes.Encrypt(painText);
+            var encrypted = aes.Encrypt(plainText);
             var decrypted = aes.Decrypt(encrypted);
 
-            decrypted.Should().Be(painText, "the decrypted string should be unchanged from the original string");
+            decrypted.Should().Be(plainText, "the decrypted string should be unchanged from the original string");
         }
     }
 }
